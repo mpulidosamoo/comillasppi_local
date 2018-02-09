e20c4848968dda27d57aa66ceb864fd7037debd8
mpulidosamoo <mpulido@pentec.es>
2018-02-09 11:23:55 +0100

    v1.1

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 /**
  * Version details.
  *
- * @package local_comillasppi
+ * @package local_comillasppi5
  * @copyright  2018 Planificacion de Entornos Tecnologicos SL
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
